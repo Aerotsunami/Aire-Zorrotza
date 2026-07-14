f1403564191d928787e6b0c5ffe8b773e2b14963
Aerotsunami <gradusteam@gmail.com>
2026-07-14 11:17:27 +0200

    Restore complete assets and compact mobile sensor grid

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
-const CACHE = 'zorrotza-aire-v1.3.0';
+const CACHE = 'zorrotza-aire-v1.3.1';
 const APP_SHELL = [
-  './', './index.html', './styles.css?v=1.3.0', './app.js?v=1.3.0', './manifest.webmanifest',
+  './', './index.html', './styles.css?v=1.3.1', './app.js?v=1.3.1', './manifest.webmanifest',
   './icons/icon.svg', './icons/icon-192.png', './icons/icon-512.png'
 ];
 
